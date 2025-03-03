9720765e2bf33b000e80b2ba5764a9944047d96d
Junior <jumabunjhb024@student.wethinkcode.co.za>
2025-03-03 14:35:32 +0200

    Add links to nav bar buttons

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import Icon from '@mdi/react';
 import { mdiHomeOutline } from '@mdi/js';
 import { mdiMessageOutline } from '@mdi/js';
@@ -10,26 +12,32 @@ export default function NavigationBar() {
     const buttons = [
         {
             'path': mdiHomeOutline,
-            'title': 'Home'
+            'title': 'Home',
+            'href': '/'
         },
         {
             'path': mdiMessageOutline,
-            'title': 'Inbox'
+            'title': 'Inbox',
+            'href': '/inbox'
         },
         {
             'path': mdiFileDocumentOutline,
-            'title': 'Applics'
+            'title': 'Applics',
+            'href': '/applications'
         },
         {
             'path': mdiAccountOutline,
-            'title': 'Profile'
+            'title': 'Profile',
+            'href': '/profile'
         }
     ];
 
     return (
         <div className='bg-black mx-auto pt-3 pb-1 px-5 rounded-lg flex gap-x-4 max-w-sm justify-evenly'>
             {buttons.map((btn, index) => 
-                <div key={index} className='text-center'>
+                <Link
+                    href={btn.href} 
+                    key={index} className='text-center'>
                     <div className='bg-lavendar size-min rounded-lg p-1'>
                         <Icon
                             path={btn.path}
@@ -38,7 +46,7 @@ export default function NavigationBar() {
                         />
                     </div>
                     <span className='text-lavendar m-0'>{btn.title}</span>
-                </div>
+                </Link>
             )}
         </div>
     );
